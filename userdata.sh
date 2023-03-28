9d2339e3eab25d807fb7751a89599a3a08b64dbc
sahil bhanvadiya <81022413+sahil-bhanvadiya@users.noreply.github.com>
2023-03-28 13:08:10 +0530

    Add files via upload

---
+++ userdata.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+sudo apt update
+sudo apt install nodejs -y
+node -v
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install --lts
+nvm --version
