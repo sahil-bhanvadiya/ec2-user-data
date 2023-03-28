1cde97dcebd2da70ba689ee77967bb180affa503
sahil bhanvadiya <81022413+sahil-bhanvadiya@users.noreply.github.com>
2023-03-29 00:11:21 +0530

    update

--- userdata.sh
+++ userdata.sh
@@ -1,11 +1,16 @@
-#! /bin/bash
-
-sudo apt update
-sudo apt install nodejs -y
-node -v
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+#!/bin/bash
+apt-get -y update
+cat > /tmp/subscript.sh << EOF
+echo "Setting up NodeJS Environment"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.9/install.sh | bash
+echo 'export NVM_DIR="/home/ubuntu/.nvm"' >> /home/ubuntu/.bashrc
+echo '[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm' >> /home/ubuntu/.bashrc
+. /home/ubuntu/.nvm/nvm.sh
+. /home/ubuntu/.profile
+. /home/ubuntu/.bashrc
 nvm install --lts
-nvm --version
+nvm ls
+EOF
+
+chown ubuntu:ubuntu /tmp/subscript.sh && chmod a+x /tmp/subscript.sh
+sleep 1; su - ubuntu -c "/tmp/subscript.sh"
